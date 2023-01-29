469de519cdc9e688ee8a3d237af8b96fded00d52
James Moore <jamesmooredeveloper@gmail.com>
2023-01-29 15:00:47 +0000

    thumbnail in file panel

--- SDMetaUI/Models/PngFileViewModel.cs
+++ SDMetaUI/Models/PngFileViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace SDMetaUI.Models
+﻿using SDMetaTool;
+
+namespace SDMetaUI.Models
 {
 	public class PngFileViewModel
 	{
@@ -10,5 +12,31 @@
 		public string FullPromptHash { get; set; }
 		public bool Expanded { get; set; }
 		public string Tooltip { get; set; }
+
+		public static PngFileViewModel FromModel(PngFile p)
+		{
+			return new PngFileViewModel()
+			{
+				Filename = p.Filename,
+				ImageUrl = $"/images/thumb/{Base64Encode(p.Filename)}",
+				LastUpdated = p.LastUpdated,
+				Length = p.Length,
+				Prompt = p.Parameters?.Prompt ?? "",
+				FullPromptHash = p.Parameters?.Prompt + p.Parameters?.NegativePrompt ?? "",
+				Tooltip = $@"
+					<small>
+					<strong>Model:</strong> {p.Parameters?.Model ?? ""}<br/>
+					<strong>Hash:</strong> {p.Parameters?.ModelHash ?? ""}<br/>
+					<strong>Sampler:</strong> {p.Parameters?.Sampler ?? ""}<br/>
+					<strong>Date:</strong> {p.LastUpdated}
+					</small>",
+			};
+		}
+
+		private static string Base64Encode(string plainText)
+		{
+			var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+			return System.Convert.ToBase64String(plainTextBytes);
+		}
 	}
 }
