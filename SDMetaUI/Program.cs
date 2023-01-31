beea1c683cd2f6e3cb2bcc080ff38a1b85fb031e
James Moore <jamesmooredeveloper@gmail.com>
2023-01-31 21:08:17 +0000

    basic rescan

--- SDMetaUI/Program.cs
+++ SDMetaUI/Program.cs
@@ -1,5 +1,8 @@
 using Havit.Blazor.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using SDMetaTool;
 using SDMetaTool.Cache;
+using SDMetaTool.Processors;
 using System.IO.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,8 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<IFileSystem, FileSystem>();
-builder.Services.AddSingleton<IPngFileDataSource, JsonDataSource>();
+AddCustomServices(builder);
 builder.Services.AddControllers();
 builder.Services.AddHxServices();        // <------ ADD THIS LINE
 var app = builder.Build();
@@ -16,9 +18,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -31,3 +33,16 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
+
+static void AddCustomServices(WebApplicationBuilder builder)
+{
+	builder.Services.AddSingleton<IFileSystem, FileSystem>();
+	builder.Services.AddSingleton<IPngFileDataSource, JsonDataSource>();
+	builder.Services.AddSingleton<IFileLister, FileLister>();
+	builder.Services.AddSingleton<IPngFileLoader>(x => 
+		new CachedPngFileLoader(x.GetRequiredService<IFileSystem>(),
+		new PngFileLoader(x.GetRequiredService<IFileSystem>()),
+		x.GetRequiredService<IPngFileDataSource>()
+		));
+	builder.Services.AddSingleton<Rescan>();
+}
