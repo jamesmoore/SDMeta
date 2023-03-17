9c1bd61cfd0c1d3ff63de44016676777a1e54ecb
James Moore <jamesmooredeveloper@gmail.com>
2023-03-17 08:12:57 +0000

    reduce build warnings

--- SDMetaUI/Models/ModelSummaryViewModel.cs
+++ SDMetaUI/Models/ModelSummaryViewModel.cs
@@ -1,11 +1,27 @@
-﻿namespace SDMetaUI.Models
+﻿using SDMetaTool;
+
+namespace SDMetaUI.Models
 {
 	public class ModelSummaryViewModel
 	{
-		public string Id { get; set; }
-		public string Model { get; set; }
-		public string ModelHash { get; set; }
-		public int Count { get; set; }
-		public string Text { get; set; }
+		public ModelSummaryViewModel() {
+			Id = 0; 
+			Text = "-- all models --";
+		}
+
+		public ModelSummaryViewModel(ModelSummary p, int id)
+		{
+			Id = id;
+			Count = p.Count;
+			Model = p.Model;
+			ModelHash = p.ModelHash;
+			Text = (p.ModelHash ?? "<empty>") + " (" + (p.Model ?? "<no name>") + ") [" + p.Count + "]";
+		}
+
+		public int Id { get; }
+		public string? Model { get; }
+		public string? ModelHash { get; }
+		public int Count { get; }
+		public string Text { get; }
 	}
 }
